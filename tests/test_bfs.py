8fad40a4d53f137747509a9a55b1792fcec0ed8e
Pavel Alimov <42354122+Krekep@users.noreply.github.com>
2023-03-12 23:02:33 +0300

    Remove pytest parametrize

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -4,45 +4,49 @@ from project import matrix_bfs
 from project.utils import graph_from_edges_list
 
 
-@pytest.mark.parametrize(
-    "graph, start, expected",
-    [
-        (graph_from_edges_list(3, [(0, 1), (1, 2)]), 0, [0, 1, 2]),
-        (
-            graph_from_edges_list(4, [(0, 1), (1, 2), (2, 3), (0, 3)]),
-            0,
-            [0, 1, 2, 1],
+_metaparams = [
+    (graph_from_edges_list(3, [(0, 1), (1, 2)]), 0, [0, 1, 2]),
+    (
+        graph_from_edges_list(4, [(0, 1), (1, 2), (2, 3), (0, 3)]),
+        0,
+        [0, 1, 2, 1],
+    ),
+    (graph_from_edges_list(2, []), 0, [0, -1]),
+    (
+        graph_from_edges_list(
+            3,
+            [
+                (0, 1),
+                (1, 2),
+                (2, 0),
+            ],
+            is_undirected=True,
         ),
-        (graph_from_edges_list(2, []), 0, [0, -1]),
-        (
-            graph_from_edges_list(
-                3,
-                [
-                    (0, 1),
-                    (1, 2),
-                    (2, 0),
-                ],
-                is_undirected=True,
-            ),
-            0,
-            [0, 1, 1],
+        0,
+        [0, 1, 1],
+    ),
+    (
+        graph_from_edges_list(
+            3,
+            [
+                (0, 1),
+                (1, 2),
+                (2, 0),
+            ],
+            is_undirected=False,
         ),
-        (
-            graph_from_edges_list(
-                3,
-                [
-                    (0, 1),
-                    (1, 2),
-                    (2, 0),
-                ],
-                is_undirected=False,
-            ),
-            0,
-            [0, 1, 2],
-        ),
-        (graph_from_edges_list(0, []), None, []),
-    ],
-)
-def test_bfs(graph, start, expected):
-    actual = matrix_bfs.bfs(graph, start)
-    assert actual == expected
+        0,
+        [0, 1, 2],
+    ),
+    (graph_from_edges_list(0, []), None, []),
+]
+
+
+def test_bfs():
+    for params in _metaparams:
+        graph = params[0]
+        start = params[1]
+        expected = params[2]
+        
+        actual = matrix_bfs.bfs(graph, start)
+        assert actual == expected
